ca61815f93716e55e99475f78eb4f857b9ba6562
phoBowl <tdnam39@gmail.com>
2018-07-09 16:09:57 +1000

    shapeArea

---
+++ Practice/Arrays/shapeArea/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+
+namespace shapeArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(shape(5));
+            //return shape(1);
+            
+        }
+
+        public static int shape (int n){
+            return n*n + (n-1)*(n-1);
+        }
+        // 1 : 1 
+        // 2 : 5  +4  2*2+1
+        // 3 : 13 +8  3*3
+        // 4 : 25 +12 4*4+3^2 
+    }
+}
+
